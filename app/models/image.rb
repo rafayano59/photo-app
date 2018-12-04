29d06b8ce1320caa0ccb597f55d01f97a32f8fe6
Rails Udemy Course <railsudemycourse@gmail.com>
2018-12-04 15:08:14 +0000

    Image upload, size validations

---
+++ app/models/image.rb
@@ -0,0 +1,13 @@
+class Image < ApplicationRecord
+  belongs_to :user
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
+  
+  private
+  
+  def picture_size
+    if picutre.size > 5.megabytes
+      erros.add(:picture, "should be less than 5MB")
+    end
+  end
+end
